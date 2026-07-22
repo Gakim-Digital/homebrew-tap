907f511dee03c05e15f1f1b9fea727971c80a562
gachez <briangach98@gmail.com>
2026-07-22 11:05:28 +0300

    Update Dexter Bridge formula to version 0.5.1 and fix environment variable path

--- Formula/dexter-bridge.rb
+++ Formula/dexter-bridge.rb
@@ -1,8 +1,8 @@
 class DexterBridge < Formula
   desc "Local Claude Code connector for the Dexter Framer plugin"
   homepage "https://instawebai.com/dexter-bridge"
-  url "https://registry.npmjs.org/@gakim-digital/dexter-bridge/-/dexter-bridge-0.5.0.tgz"
-  sha256 "f6ac8e0238627584e642503f4e7fef374688cca650e533b58a19fc8abc5ef0d5"
+  url "https://registry.npmjs.org/@gakim-digital/dexter-bridge/-/dexter-bridge-0.5.1.tgz"
+  sha256 "bacec58c4ab8ba055190168f41b819054e4f36389fbef153dca0dcf5302a0ce7"
   license :cannot_represent
 
   depends_on "node@22"
@@ -19,7 +19,7 @@ class DexterBridge < Formula
     process_type :background
     log_path var/"log/dexter-bridge.log"
     error_log_path var/"log/dexter-bridge.error.log"
-    environment_variables PATH: std_service_path_env,
+    environment_variables PATH: "#{Formula["node@22"].opt_bin}:#{std_service_path_env}",
                           DEXTER_BRIDGE_INSTALL_METHOD: "homebrew"
   end
 
